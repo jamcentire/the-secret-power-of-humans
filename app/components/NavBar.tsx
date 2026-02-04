486a58bc5545ab7752a41eb9028cc3b6ade0b962
Jacob <jmcentir@gmail.com>
2026-02-04 17:58:30 -0500

    Styled navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,27 @@
 import Link from 'next/link';
 
+interface NavBarTabProps {
+  displayText: string;
+  pageRef: string;
+}
+
+const NavBarTab = (props: NavBarTabProps) => {
+  return (
+    <Link href={props.pageRef}
+      className='w-1/10 h-1/2 text-xl rounded-3xl flex items-center justify-center hover:border-2 border-yellow-300 hover:shadow-[0_0_10px_8px_rgba(253,224,71,0.8)]'
+    >
+      {props.displayText}
+    </Link>
+  )
+}
+
 export const NavBar = () => {
   return (
-    <>
-      <Link href='/'>Home</Link>
-      <Link href='/character-creator'>Character creator</Link>
-    </>
+    <div
+      className='flex flex-row w-full h-22 justify-center bg-blue-300 shadow-md items-center gap-8'
+    >
+    <NavBarTab displayText='Home' pageRef='/' />
+    <NavBarTab displayText='Character Creator' pageRef='/character-creator' />
+    </div>
   )
 }
